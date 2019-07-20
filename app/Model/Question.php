52e503ddefcb7a847d7df00a017eef7d89748cb8
HAZEM <hazemabuelala10@gmail.com>
2019-07-20 20:44:27 +0200

    RelashionShip

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -3,8 +3,25 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Reply;
+use App\Category;
 
 class Question extends Model
 {
-    //
+    //relation with user
+    public function user()
+    {
+        return $this->belongsTo('User::class');
+    }
+    //relation with replies
+    public function replies()
+    {
+        return $this->hasMany('Reply::class');
+    }
+    //relation with category
+    public function category()
+    {
+        return $this->belongsTo('Category::class');
+    }
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -4,7 +4,25 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+use App\Quesyion;
+use App\Like;
+
 class Reply extends Model
 {
-    //
+    //relation with Like
+    public function likes()
+    {
+        return $this->hasMany('Like::class');
+    }
+    //relation with question
+    public function question()
+    {
+        return $this->belongsTo('Question::class');
+    }
+    //relation with User
+    public function user()
+    {
+        return $this->belongsTo('User::class');
+    }
 }
