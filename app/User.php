e25d56a789dd4a1578c50d745e83e0e9c5cad6a6
HAZEM <hazemabuelala10@gmail.com>
2019-07-23 23:06:07 +0200

    add JWT Authontications library in project

--- app/User.php
+++ app/User.php
@@ -8,8 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 
 use App\Model\Question;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -42,5 +43,26 @@ class User extends Authenticatable
 
     public function questions() {
         return $this->hasMany(Question::class);
+    }
+
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
